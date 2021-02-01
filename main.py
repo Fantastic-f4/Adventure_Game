8df29dedb7cf3cd8ed5d8a695e697a296e1ad068
Peaceoni49 <47338298+Peaceoni49@users.noreply.github.com>
2021-02-01 13:37:19 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-def option1():
-    print("The screams get louder and closer")
-    print("OH! NO! its a really big monster")
-    print("ITS HURTS! SOMEONE HELP ME!")
-    print("game over")
-    playagain = input (" Whould you like to play again? ")
-    if playagain == "YES" or "yes":
-        intro()
+
+def intro():
+    print("a = Force the door")
+    print("b  = Search the room")
+    print("c = Go back to sleep")
+    start = input("Would you like to go back to sleep, search the room or force the door?  ").lower()
+
+
+
+    if start == "a" :
+        print("You stand up, shaking from the realisation that you have been captured and run at the door.")
+        print(" BANG. The door doesn’t budge but creates an almighty noise that bounces through")
+        print("the castle. You hear a scream like you have never heard before...")
+        print("What is this monster…. ")
+        option1()
+        
+
+    elif start == "b":
+        print("You struggle to your feet to search the room, horrified by what you see,")
+        print(" you closer inspect the skeleton. You pick up the hand shackles from one of the")
+        print(" skeleton and the arm breaks clean off. CRACK. This startles you but reveals an ")
+        print("iron shackle pin that you can use to pick the lock. After picking the lock,")
+        print(" you slowly open the door and creep out of the dungeon.")
+        print("A long and winding staircase takes you to the bottom of the turret to another wooden arched door")
+        
+    elif start == "c":
+        print("Go back to sleep and have a nice dream OR MAYBE!!!!!!")
+        print("     ######################")
+        print("     |                    |")
+        print("     |     HAHAHAHAHA     |")
+        print("     |     HAHAHAHAHA     |")
+        print("     |     HAHAHAHAHA     |")
+        print("     |                    |")
+        print("     ######################")
+
+        def option1():
+            print("The screams get louder and closer")
+            print("OH! NO! its a really big monster")
+            print("ITS HURTS! SOMEONE HELP ME!")
+            print("game over")
+            playagain = input (" Whould you like to play again? ")
+            if playagain == "NO" or "no":
+                print (Goodbye)
+            elif playagain == "YES" or "yes":
+                intro()
 
 
 #Main Function
 
-def intro():
+
 print("You wake up on a cold stone floor.")
 print( "Moonlight creeping through an iron barred window reveals skeletons chained to the walls.")
 print(" The room is small and round with one arched door.")
@@ -32,37 +69,13 @@ print("     |     HAHAHAHAHA     |")
 print("     |                    |")
 print("     ######################")
 
-print()
-startgame = input("Would you like to go back to sleep, search the room or force the door?  ").lower()
+startgame = input("Would you like to start the game? (Y?N): ")
+if startgame == "n" or startgame == "N":
+    print ("Maybe some other time. Bye for now...")
+elif startgame == "y" or startgame == "Y":
+    print("Welcome to hells's doom!")
+    intro()
 
-print("a = Force the door")
-print("b  = Search the room")
-print("c = Go back to sleep")
-
-if startgame == "a" :
-    print("You stand up, shaking from the realisation that you have been captured and run at the door.")
-    print(" BANG. The door doesn’t budge but creates an almighty noise that bounces through")
-    print("the castle. You hear a scream like you have never heard before...")
-    print("What is this monster…. ")
-     option1()
-
-elif startgame == "b":
-    print("You struggle to your feet to search the room, horrified by what you see,")
-    print(" you closer inspect the skeleton. You pick up the hand shackles from one of the")
-    print(" skeleton and the arm breaks clean off. CRACK. This startles you but reveals an ")
-    print("iron shackle pin that you can use to pick the lock. After picking the lock,")
-    print(" you slowly open the door and creep out of the dungeon.")
-    print("A long and winding staircase takes you to the bottom of the turret to another wooden arched door")
-    option2()
-elif startgame == "c":
-    print("Go back to sleep and have a nice dream OR MAYBE!!!!!!")
-    print("     ######################")
-    print("     |                    |")
-    print("     |     HAHAHAHAHA     |")
-    print("     |     HAHAHAHAHA     |")
-    print("     |     HAHAHAHAHA     |")
-    print("     |                    |")
-    print("     ######################")
 
     
 
