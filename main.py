a1c0a75e7f0e85b025f9c410e0a60e8c87769dab
Peaceoni49 <47338298+Peaceoni49@users.noreply.github.com>
2021-02-01 12:11:47 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ def option1():
     print("OH! NO! its a really big monster")
     print("ITS HURTS! SOMEONE HELP ME!")
     print("game over")
-    input (" Whould you like to play again? ")
+    playagain = input (" Whould you like to play again? ")
+    if playagain == "YES" or "yes":
+        intro()
+
 
 #Main Function
 
+def intro():
 print("You wake up on a cold stone floor.")
 print( "Moonlight creeping through an iron barred window reveals skeletons chained to the walls.")
 print(" The room is small and round with one arched door.")
@@ -38,8 +42,8 @@ print("c = Go back to sleep")
 if startgame == "a" :
     print("You stand up, shaking from the realisation that you have been captured and run at the door.")
     print(" BANG. The door doesn’t budge but creates an almighty noise that bounces through")
-     print("the castle. You hear a scream like you have never heard before...")
-     print("What is this monster…. ")
+    print("the castle. You hear a scream like you have never heard before...")
+    print("What is this monster…. ")
      option1()
 
 elif startgame == "b":
