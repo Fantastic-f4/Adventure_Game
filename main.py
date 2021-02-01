7e21cfdb341787a969a0cc3f812f9986374a2995
Peaceoni49 <47338298+Peaceoni49@users.noreply.github.com>
2021-02-01 11:49:31 +0000

    Add files via upload
    1st code. Introduction to the game.

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+#Main Function
+
+print("You wake up on a cold stone floor.")
+print( "Moonlight creeping through an iron barred window reveals skeletons chained to the walls.")
+print(" The room is small and round with one arched door.")
+
+print("You hear a voice fill the room")
+print("In fear you reply")
+
+name = input("What is your name: ")
+print()
+print()
+print(name + "! HOPE YOU SLEPT WELL!!!!!")
+
+
+print("     ######################")
+print("     |                    |")
+print("     |     HAHAHAHAHA     |")
+print("     |     HAHAHAHAHA     |")
+print("     |     HAHAHAHAHA     |")
+print("     |                    |")
+print("     ######################")
+
+print()
+startgame = input("Would you like to go back to sleep? or escape this smelly dungeon?  ")
+
+
